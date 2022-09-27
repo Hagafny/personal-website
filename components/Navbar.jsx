cba1e11a4d362856f547c99b06d93ae9f52c8509
Hagafny <ronhagafny20@gmail.com>
2022-09-27 19:34:00 +0300

    refactored pages

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,38 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { useRouter } from "next/router";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+// import { useRouter } from 'next/router';
+import NavLogo from '../public/assets/navLogo.png'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
-  const [navBg, setNavBg] = useState("#ecf0f3");
-  const [linkColor, setLinkColor] = useState("#1f2937");
-  const router = useRouter();
+  const [navBg, setNavBg] = useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+  // const [position, setPosition] = useState('fixed')
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   if (
+  //     router.asPath === '/property' ||
+  //     router.asPath === '/crypto' ||
+  //     router.asPath === '/netflix' ||
+  //     router.asPath === '/twitch'
+  //   ) {
+  //     setNavBg('transparent');
+  //     setLinkColor('#ecf0f3');
+  //   } else {
+  //     setNavBg('#ecf0f3');
+  //     setLinkColor('#1f2937');
+  //   }
+  // }, [router]);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   useEffect(() => {
     const handleShadow = () => {
@@ -21,80 +42,88 @@ const Navbar = () => {
         setShadow(false);
       }
     };
-
-    const listener = window.addEventListener("scroll", handleShadow);
-
-    return () => window.removeEventListener("scroll", listener);
+    window.addEventListener('scroll', handleShadow);
   }, []);
-
-  useEffect(() => {
-
-    if (router.asPath === "/project/property") {
-      setNavBg("transparent");
-      setLinkColor("#ecf0f3");
-    } else {
-      setNavBg("#ecf0f3");
-      setLinkColor("#1f2937");
-    }
-  }, [router]);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
 
   return (
     <div
       style={{ backgroundColor: `${navBg}` }}
       className={
         shadow
-          ? "fixed w-full h-20 shadow-xl z-[100]"
-          : "fixed w-full h-20 z-[100]"
+          ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
+          : 'fixed w-full h-20 z-[100]'
       }
     >
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Link href='/'>
-          <Image
-            src='/../public/assets/navLogo.png'
-            alt='/'
-            width='125'
-            height='50'
-          />
+          <a>
+            <Image
+              src={NavLogo}
+              alt='/'
+              width='125'
+              height='50'
+              className='cursor-pointer'
+            />
+          </a>
         </Link>
         <div>
           <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
-            <NavbarItem href='/'>Home</NavbarItem>
-            <NavbarItem href='/#about'>About</NavbarItem>
-            <NavbarItem href='/#skills'>Skills</NavbarItem>
-            <NavbarItem href='/#projects'>Projects</NavbarItem>
-            <NavbarItem href='/#contact'>Contact</NavbarItem>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#about'>About</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#skills'>Skills</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#projects'>Projects</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/resume'>Resume</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#contact'>Contact</Link>
+            </li>
           </ul>
-          <div onClick={handleNav} className='md:hidden'>
+          {/* Hamburger Icon */}
+          <div
+            style={{ color: `${linkColor}` }}
+            onClick={handleNav}
+            className='md:hidden'
+          >
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
+      {/* Mobile Menu */}
+      {/* Overlay */}
       <div
         className={
-          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+          nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
         }
       >
+        {/* Side Drawer Menu */}
         <div
           className={
             nav
-              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
           }
         >
           <div>
             <div className='flex w-full items-center justify-between'>
               <Link href='/'>
-                <Image
-                  src='/../public/assets/navLogo.png'
-                  width='87'
-                  height='35'
-                  alt='/'
-                />
+                <a>
+                  <Image
+                    src={NavLogo}
+                    width='87'
+                    height='35'
+                    alt='/'
+                  />
+                </a>
               </Link>
               <div
                 onClick={handleNav}
@@ -105,7 +134,7 @@ const Navbar = () => {
             </div>
             <div className='border-b border-gray-300 my-4'>
               <p className='w-[85%] md:w-[90%] py-4'>
-                Let's build something legendary together
+                Let&#39;s build something legendary together
               </p>
             </div>
           </div>
@@ -131,6 +160,11 @@ const Navbar = () => {
                   Projects
                 </li>
               </Link>
+              <Link href='/resume'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
+                  Resume
+                </li>
+              </Link>
               <Link href='/#contact'>
                 <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Contact
@@ -139,35 +173,49 @@ const Navbar = () => {
             </ul>
             <div className='pt-40'>
               <p className='uppercase tracking-widest text-[#5651e5]'>
-                Let's connect
+                Let&#39;s Connect
               </p>
               <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
-                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                  <FaLinkedinIn />
-                </div>
-                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                  <FaGithub />
-                </div>
-                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                  <AiOutlineMail />
-                </div>
-                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                  <BsFillPersonLinesFill />
-                </div>
+                <a
+                  href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href='https://github.com/fireclint'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                </a>
+                <Link href='/#contact'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href='/resume'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <BsFillPersonLinesFill />
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
-};
-
-const NavbarItem = ({ href = "", children }) => {
-  return (
-    <Link href={href}>
-      <li className='ml-10 text-small uppercase hover:border-b'>{children}</li>
-    </Link>
   );
 };
 
